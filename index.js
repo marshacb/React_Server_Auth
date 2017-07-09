b9ebec57bb418822864136ab13c35661e257cffa
Cameron <marshacb@Camerons-MacBook-Pro.local>
2017-07-09 00:06:38 -0400

    set up node server to run on port 3090

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express(); //express instance
+
+//App Setup, get express working
+
+
+//Server Setup, get express communicating to outside world
+const port = process.env.PORT || 3090;
+const server = http.createServer(app); //http, native node library
+server.listen(port);
+console.log("Server listening on:", port);
